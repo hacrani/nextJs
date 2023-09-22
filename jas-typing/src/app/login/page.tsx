0342db4473985f6c7c7cc5aeb72180c621f26004
Rani <yrani1916@gmail.com>
2023-09-22 18:52:22 +0530

    created new project

---
+++ jas-typing/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import styles from '@/style/page.module.css'
+
+export default function Login() {
+    return (
+        <div className={styles.main}>
+            <p style={{ color: 'black', backgroundColor: 'pink' }}>Login Page</p>
+            <p>hdfalfnd jfklda </p>
+
+            <p className={styles.myClass}>This is a red text</p>
+            <p id='myId'>hfdladks fdfl</p>
+        </div>
+    );
+}
