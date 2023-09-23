49ffad22912aa4d729850f8dcf504fc6456c8e64
Rani <yrani1916@gmail.com>
2023-09-23 12:47:34 +0530

    Linking & navigations

--- jas-typing/src/app/about/me/page.tsx
+++ jas-typing/src/app/about/me/page.tsx
@@ -1,5 +1,16 @@
-export default function me() {
+"use client"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Me() {
+    const router = useRouter()
+
     return (
-        <h1>About Me Page</h1>
+        <div>
+            <h1>hello</h1>
+            <Link href='/login' >Login page</Link>
+            <br/>
+            <button onClick={() => router.push('/about')}>About Page </button>
+        </div>
     );
 }
--- jas-typing/src/app/login/page.tsx
+++ jas-typing/src/app/login/page.tsx
@@ -1,10 +1,12 @@
-import styles from '@/style/page.module.css'
+import styles from '@/style/page.module.css';
+import Link from 'next/link';
 
 export default function Login() {
     return (
         <div className={styles.main}>
             <p style={{ color: 'black', backgroundColor: 'pink' }}>Login Page</p>
             <p>hdfalfnd jfklda </p>
+            <Link href='/about/me' >About page</Link>
 
             <p className={styles.myClass}>This is a red text</p>
             <p id={styles.myId}>hfdladks fdfl</p>
